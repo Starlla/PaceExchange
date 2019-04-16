6489e9ab79a9dac1c0bca19a37232f4300d14e76
Claire <claire.cy.t@gmail.com>
2019-04-16 13:07:43 -0400

    add vector asset for profile;make OnClickListen on icon instead of on text

--- app/src/main/java/com/example/barterapp/MainActivity.java
+++ app/src/main/java/com/example/barterapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -15,8 +16,9 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity implements ProfileFragment.ProfileFragmentButtonClickHandler{
 
     private TextView userTest;
-    private TextView currentTabView;
+    private View currentTabView;
     private TextView profileTab;
+    ImageView profileIconView;
     ProfileFragment profileFragment;
     Fragment currentFragment;
     String uid;
@@ -29,6 +31,7 @@ public class MainActivity extends AppCompatActivity implements ProfileFragment.P
         userTest = findViewById(R.id.test_user);
 
         profileTab = findViewById(R.id.profile_tab);
+        profileIconView =findViewById(R.id.profile_tab_icon);
 
         Intent intent = getIntent();
         Bundle bd = intent.getExtras();
@@ -58,22 +61,23 @@ public class MainActivity extends AppCompatActivity implements ProfileFragment.P
 
         View.OnClickListener tabListener = view -> {
 
-            if(currentTabView != null)
-                currentTabView.setBackgroundColor(Color.WHITE);
-            currentTabView = currentTabView == null ? profileTab : (TextView)view;
-            currentTabView .setBackgroundColor(getResources().getColor(R.color.colorSelect));
+//            if(currentTabView != null)
+//                currentTabView.setBackgroundColor(Color.WHITE);
+            currentTabView = currentTabView == null ? profileIconView : view;
+//            currentTabView .setBackgroundColor(getResources().getColor(R.color.colorSelect));
 
 
             int currentTabId = currentTabView == null ? 0 : currentTabView.getId();
             switch (currentTabId) {
-                case R.id.profile_tab:
+                case R.id.profile_tab_icon:
                     currentFragment = profileFragment;
+                    profileIconView.setImageResource(R.drawable.ic_person_black_24dp);
                     break;
             }
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, currentFragment).commit();
         };
         //add above listener to tabs
-        findViewById(R.id.profile_tab).setOnClickListener(tabListener);
+        findViewById(R.id.profile_tab_icon).setOnClickListener(tabListener);
     }
 
 
