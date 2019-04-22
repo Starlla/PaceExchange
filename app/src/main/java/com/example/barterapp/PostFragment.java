9f3a8ce05c60bad33e4471fac1b67f893df909c1
Claire <claire.cy.t@gmail.com>
2019-04-21 22:36:02 -0400

    Add select photo dialog

--- app/src/main/java/com/example/barterapp/PostFragment.java
+++ app/src/main/java/com/example/barterapp/PostFragment.java
@@ -6,11 +6,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class PostFragment extends Fragment {
+
+    ImageView mPostImage;
 
     interface ShopFragmentButtonClickHandler{
         void signOutButtonClicked();
@@ -34,6 +38,16 @@ public class PostFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_post, container, false);
+        mPostImage = view.findViewById(R.id.post_image);
+
+        mPostImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SelectPhotoDialog dialog = new SelectPhotoDialog();
+                dialog.show(getFragmentManager(), getString(R.string.dialog_select_photo));
+                dialog.setTargetFragment(PostFragment.this, 1);
+            }
+        });
 
         return view;
     }
