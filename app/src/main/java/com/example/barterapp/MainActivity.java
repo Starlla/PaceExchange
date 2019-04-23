1ec8542a6760059c31fd08227a523bb83d707455
Claire <claire.cy.t@gmail.com>
2019-04-22 21:09:40 -0400

    Make upload photo functional

--- app/src/main/java/com/example/barterapp/MainActivity.java
+++ app/src/main/java/com/example/barterapp/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.barterapp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.VectorDrawable;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,6 +21,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity implements ProfileFragment.ProfileFragmentButtonClickHandler, ShopFragment.ShopFragmentButtonClickHandler{
+    private static final int REQUEST_CODE = 1;
 
     private TextView userTest;
     private ImageView currentTabView;
@@ -52,8 +59,8 @@ public class MainActivity extends AppCompatActivity implements ProfileFragment.P
 //            userTest.setText(email + "  " + uid);
         }
 
-        configureFragment();
-        addTabClickListeners();
+        verifyPermissions();
+
     }
 
     private  void configureFragment(){
@@ -113,5 +120,30 @@ public class MainActivity extends AppCompatActivity implements ProfileFragment.P
         FirebaseAuth.getInstance().signOut();
         Intent intent = new Intent(MainActivity.this, SignInActivity.class);
         startActivity(intent);
+    }
+
+    private void verifyPermissions(){
+        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[0]) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[1]) == PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                permissions[2]) == PackageManager.PERMISSION_GRANTED){
+            configureFragment();
+            addTabClickListeners();
+        }else{
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    permissions,
+                    REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        verifyPermissions();
     }
 }
--- app/src/main/java/com/example/barterapp/PostFragment.java
+++ app/src/main/java/com/example/barterapp/PostFragment.java
@@ -1,6 +1,7 @@
 package com.example.barterapp;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -12,22 +13,44 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-public class PostFragment extends Fragment {
+public class PostFragment extends Fragment implements SelectPhotoDialog.OnPhotoSelectedListener{
 
-    ImageView mPostImage;
+    private static final String TAG = "PostFragment";
+    private Bitmap mSelectedBitmap;
+    private Uri mSelectedUri;
+    private ImageView mPostImage;
 
-    interface ShopFragmentButtonClickHandler{
+    @Override
+    public void getImagePath(Uri imagePath) {
+        Log.d(TAG, "getImagePath: setting the image to imageview");
+        UniversalImageLoader.setImage(imagePath.toString(), mPostImage);
+
+        mSelectedBitmap = null;
+        mSelectedUri = imagePath;
+    }
+
+    @Override
+    public void getImageBitmap(Bitmap bitmap) {
+        Log.d(TAG, "getImageBitmap: setting the image to imageview");
+        mPostImage.setImageBitmap(bitmap);
+
+        mSelectedUri = null;
+        mSelectedBitmap = bitmap;
+
+    }
+
+    interface PostFragmentButtonClickHandler{
         void signOutButtonClicked();
     }
 
-    ShopFragmentButtonClickHandler mClickHandler;
+    PostFragmentButtonClickHandler mClickHandler;
     String mUid;
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
         try{
-            mClickHandler = (ShopFragmentButtonClickHandler) context;
+            mClickHandler = (PostFragmentButtonClickHandler) context;
         }catch(ClassCastException e){
             new ClassCastException("the activity that  this fragment is attached to must be a FirstFragmentButtonClickHandler");
 
