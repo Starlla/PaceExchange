95e64418ee3455a0032ed3d701219cc5f05f452e
livtmy <livtmy@gmail.com>
2019-04-24 11:28:59 -0400

    Add like feature in view post fragment

--- app/src/main/java/com/example/barterapp/ViewPostFragment.java
+++ app/src/main/java/com/example/barterapp/ViewPostFragment.java
@@ -2,6 +2,7 @@ package com.example.barterapp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.BlurMaskFilter;
@@ -12,14 +13,18 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.ImageViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -27,15 +32,20 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.Iterator;
+
 public class ViewPostFragment extends Fragment {
 
     ImageView mImage;
     ImageView mPostClose;
+    ImageView mLike;
     TextView mTitle;
     TextView mDescription;
+    TextView mPostStartOffer;
 
     private String mPostId;
     private Post mPost;
+    private boolean mIsInMyLikes;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -49,26 +59,20 @@ public class ViewPostFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_view_post, container, false);
         mImage = view.findViewById(R.id.post_image);
         mPostClose = view.findViewById(R.id.post_close);
+        mLike = view.findViewById(R.id.add_watch_list);
+        mPostStartOffer = view.findViewById(R.id.post_start_offer);
         mTitle = view.findViewById(R.id.post_title);
         mDescription = view.findViewById(R.id.post_description);
 
         init();
-        hideSoftKeyboard();
-
         return view;
     }
 
     private void init() {
         getPostInfo();
-
-        mPostClose.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                getActivity().getSupportFragmentManager().popBackStack();
-            }
-        });
-
-        // Add onClickListener for "START OFFER" here
+        getLikeInfo();
+        addClickListeners();
+        hideSoftKeyboard();
     }
 
     private void getPostInfo() {
@@ -92,6 +96,83 @@ public class ViewPostFragment extends Fragment {
 
             }
         });
+    }
+
+    private void getLikeInfo() {
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+        Query query = reference.child(getString(R.string.node_likes))
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .orderByChild(getString(R.string.field_post_id)).equalTo(mPostId);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    mIsInMyLikes = true;
+                    ImageViewCompat.setImageTintList(mLike, ColorStateList.valueOf(ContextCompat
+                            .getColor(getActivity(), R.color.colorAccent)));
+                } else {
+                    mIsInMyLikes = false;
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void addClickListeners() {
+        mPostClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+        mLike.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mIsInMyLikes) {
+                    removeItemFromMyLikes();
+                    ImageViewCompat.setImageTintList(mLike, ColorStateList.valueOf(ContextCompat
+                            .getColor(getActivity(), R.color.White)));
+                } else {
+                    addItemToMyLikes();
+                    ImageViewCompat.setImageTintList(mLike, ColorStateList.valueOf(ContextCompat
+                            .getColor(getActivity(), R.color.colorAccent)));
+                }
+                mIsInMyLikes = !mIsInMyLikes;
+            }
+        });
+
+        mPostStartOffer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Add onClickListener for "START OFFER" here
+            }
+        });
+    }
+
+    private void addItemToMyLikes() {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.child(getString(R.string.node_likes))
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .child(mPostId)
+                .child(getString(R.string.field_post_id))
+                .setValue(mPostId);
+
+        Toast.makeText(getActivity(), R.string.toast_add_to_likes, Toast.LENGTH_SHORT).show();
+    }
+
+    private void removeItemFromMyLikes() {
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+        databaseReference.child(getString(R.string.node_likes))
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                .child(mPostId)
+                .removeValue();
+
+        Toast.makeText(getActivity(), R.string.toast_remove_from_likes, Toast.LENGTH_SHORT).show();
     }
 
     private void hideSoftKeyboard(){
